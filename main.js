eca2ba2c61e93b0a7d48d33ffa597ecabdbaf449
Sam AJ McRuvie <s.aj.mcruvie@gmail.com>
2021-10-25 16:09:28 +0100

    arg passing and file checking

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// imports
+const fs = require('fs');
+// gloabals
 const args = process.argv.slice(2);
 
-console.log(args)
+if(args.length > 0){
+    if (fs.existsSync(args[0])) {
+        console.log('Directory exists!');
+    } else {
+        throw new Error('Given Directory dose not exists!')
+    }
+}else{
+    throw new Error('Command line arg has not been given!')
+}
