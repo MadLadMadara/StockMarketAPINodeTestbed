2284f0bc765a16146aaaaa843d529238a9e158e1
Sam AJ McRuvie <s.aj.mcruvie@gmail.com>
2021-11-22 09:33:00 +0000

    re worked DB upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // imports
-// require('dotenv').config()
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+require('dotenv').config()
+
 import fs from 'fs';
 import mongoose from 'mongoose'
 import CompanyFacts from './models/facts/companyFacts.models.js';
@@ -8,14 +11,14 @@ import CompanyFacts from './models/facts/companyFacts.models.js';
 //const dir = process.argv.slice(2)[0];
 
 //TODO: remove so command line args work insted
-const dir = "C:\\Users\\sajmc\\OneDrive\\Documents\\Projects\\StockmarketAPI\\R&D\\testCompanyFacts"
+const dir = process.env.COMPANY_FACTS_FOLDER; 
 
 // DB connection
-// try{
-//     await mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, dbName: 'test' })
-// } catch (error){
-//     throw new Error(`unable to connect to database: ${process.env.MONGOURL}\nError:${error}`)
-// } 
+try{
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'DatabaseAlpha' })
+} catch (error){
+    throw new Error(`unable to connect to database: ${process.env.MONGO_URL}\nError:${error}`)
+} 
 
 // check if dir exists
 if(dir.length > 0){
@@ -23,6 +26,8 @@ if(dir.length > 0){
         const files = fs.readdirSync(dir);
         // files object contains all files names
         // log them on console
+        let allCompanyFacts = [];
+        console.log("File reading started"); 
         files.forEach(file => {
             let fullFilePath = dir + "\\" + file;
             console.log(fullFilePath);
@@ -32,25 +37,33 @@ if(dir.length > 0){
                 // parse JSON string to JSON object
                 const JSONdata = JSON.parse(data);
                 //use databse scheema
-                let company = new CompanyFacts({
+                let company = {
                     cik:JSONdata.cik,
                     entityName: JSONdata.entityName,
                     facts: {
-                      dei: JSONdata.facts.dei,
-                      USgaap: JSONdata.facts['us-gaap'],
-                      ifrsFull: JSONdata.facts['ifrs-full']
+                        dei: JSONdata.facts.dei,
+                        USgaap: JSONdata.facts['us-gaap'],
+                        ifrsFull: JSONdata.facts['ifrs-full']
                     } 
-                });
-                console.log(company.facts.USgaap.Assets.units.USD);
-
+                };
+                allCompanyFacts.push(company); 
             } catch (err) {
                 console.log(`Error reading file from disk: ${err}`);
             }
         });
-    
+        console.log("File reading compleated")
+        console.log("DB Date uploaded started")
+        CompanyFacts.insertMany(
+            allCompanyFacts
+        ).then(()=>{
+            console.log("DB Date uploaded compleated")
+        }).catch((error)=>{
+            console.log(error);
+        });
     } catch (err) {
         console.log(err);
     }
 }else{
     throw new Error('Command line arg has not been given!')
 }
+
